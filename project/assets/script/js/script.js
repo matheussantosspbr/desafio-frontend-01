6c8f30a3985d4c9013cd06a20cfbab6715fc6f48
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-10-18 17:16:40 -0300

    Implementação da validação se as credenciais se coincidem

--- project/assets/script/js/script.js
+++ project/assets/script/js/script.js
@@ -3,13 +3,25 @@ const username = document.querySelector('#username')
 const password = document.querySelector('#password')
 
 but.addEventListener('click', () => {
+  let msgErro = ''
   let verificacaoUsername = removerTodosOsEspacos(username.value)
   let verificacaoPassword = removerTodosOsEspacos(password.value)
 
-  // Validação para ver sse os campos está vazio
+  // Validação para ver se os campos está vazio
   if (verificacaoPassword.length == 0 || verificacaoUsername.length == 0) {
-    alert('Não é permitido ter campos vazios')
+    msgErro = 'Não é permitido ter campos vazios'
   } else {
+    // Validação se as credenciais são iguais
+    if (!(password.value == username.value)) {
+      msgErro = 'As credenciais não se coincidem'
+    }
+  }
+
+  //Validação de Erro
+  if (!msgErro) {
+    alert('está ok')
+  } else {
+    alert(msgErro)
   }
 })
 
